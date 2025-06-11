a71864c4c1e014cd82b787711df3e7baedc2a403
Bruno Silveira <57352425+M3110@users.noreply.github.com>
2025-06-11 12:10:19 -0300

    Added logger and base resiliance pipeline.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,18 +4,23 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## UNRELEASED
+### Added
+- Base resilience pipeline to automatically retry and log each attempt.
+- Logger.
+
 ## [1.0.3] - 2025-06-11
 ### Added
- - TableAttribute constructor receiving name and alias.
+- TableAttribute constructor receiving name and alias.
 
 ## [1.0.2] - 2025-06-10
 ### Fixed
- - Project informations.
+- Project informations.
 
 ## [1.0.1] - 2025-06-10
 ### Removed
- - Unnecessary build files.
+- Unnecessary build files.
 
 ## [1.0.0] - 2025-06-10
 ### Added
- - First version of the program.
+- First version of the program.
---
+++ src/MelloSilveiraTools/Infrastructure/ResiliencePipelines/BaseResiliencePipeline.cs
@@ -0,0 +1,121 @@
+﻿using MelloSilveiraTools.Infrastructure.Logger;
+using Polly;
+using Polly.RateLimiting;
+using Polly.Retry;
+using System.Runtime.CompilerServices;
+using System.Threading.RateLimiting;
+
+namespace MelloSilveiraTools.Infrastructure.ResiliencePipelines;
+
+/// <summary>
+/// Base resilience to automatically retry and log each attempt.
+/// </summary>
+public class BaseResiliencePipeline
+{
+    private static readonly ResiliencePropertyKey<string> CallerFileNamePropertyKey = new("CallerFileName");
+    private static readonly ResiliencePropertyKey<string> CallerMemberNamePropertyKey = new("CallerMemberName");
+
+    private readonly ILogger _logger;
+    private readonly ResiliencePipeline _pipeline;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="BaseResiliencePipeline"/>.
+    /// </summary>
+    /// <param name="logger">See reference at <see cref="ILogger"/>.</param>
+    /// <param name="shouldHandle">Predicate that determines whether the retry should be executed for a given outcome.</param>
+    /// <param name="settings">See reference at <see cref="ResiliencePipelineSettings"/>.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public BaseResiliencePipeline(ILogger logger, Func<RetryPredicateArguments<object>, ValueTask<bool>> shouldHandle, ResiliencePipelineSettings settings)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        _pipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions
+            {
+                ShouldHandle = shouldHandle,
+                BackoffType = settings.BackoffType,
+                Delay = TimeSpan.FromMilliseconds(settings.DelayInMilliseconds),
+                MaxRetryAttempts = settings.MaxRetryAttempts,
+                UseJitter = settings.UseJitter,
+                OnRetry = args =>
+                {
+                    ResilienceProperties contextProperties = args.Context.Properties;
+                    string? className = contextProperties.GetValue(CallerFileNamePropertyKey!, null);
+                    string? methodName = contextProperties.GetValue(CallerMemberNamePropertyKey!, null);
+
+                    // The attempt number from resilience pipeline arguments is zero-based.
+                    int attempt = args.AttemptNumber + 1;
+
+                    List<string> tags = [className, methodName];
+                    var additionalData = new Dictionary<string, object?>
+                    {
+                        { "Attempt", attempt },
+                        { nameof(args.Duration), args.Duration },
+                        { nameof(args.RetryDelay), args.RetryDelay },
+                        { nameof(args.Outcome.Result), args.Outcome.Result },
+                    };
+
+                    _logger.Warn($"Attempt '{attempt}' on '{methodName}' of '{className}'.", args.Outcome.Exception, tags, additionalData);
+                    return default;
+                }
+            })
+            // TODO: Estudar como implementar circuit breaker e rate limit nas próximas versões.
+            //.AddCircuitBreaker(new Polly.CircuitBreaker.CircuitBreakerStrategyOptions { })
+            //.AddConcurrencyLimiter(new ConcurrencyLimiterOptions { })
+            //.AddRateLimiter(new RateLimiterStrategyOptions { })
+            .Build();
+    }
+
+    /// <summary>
+    /// Encapsulates a function with a strategy that covers retriable scenarios.
+    /// </summary>
+    /// <typeparam name="T">Type returned by callback function.</typeparam>
+    /// <param name="callback">Function which will be encapsulated by the retry strategy.</param>
+    /// <param name="callerMemberName">Member name of the caller which will be logged when a retry is performed.</param>
+    /// <param name="callerFilePath">Path of the caller which will be used to get the class name to be logged when a retry is performed.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    public async ValueTask<T> ExecuteAsync<T>(Func<ResilienceContext, ValueTask<T>> callback, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "")
+    {
+        ResilienceContext context = ResilienceContextPool.Shared.Get();
+
+        try
+        {
+            // It is necessary to store the caller member name and caller file name to correctly log on retry.
+            context.Properties.Set(CallerMemberNamePropertyKey, callerMemberName);
+            context.Properties.Set(CallerFileNamePropertyKey, Path.GetFileNameWithoutExtension(callerFilePath));
+
+            // ATENTION: use async/await to correctly perform finally block.
+            return await _pipeline.ExecuteAsync(callback, context).ConfigureAwait(false);
+        }
+        finally
+        {
+            ResilienceContextPool.Shared.Return(context);
+        }
+    }
+
+    /// <summary>
+    /// Encapsulates a function with a strategy that covers retriable scenarios.
+    /// </summary>
+    /// <param name="callback">Function which will be encapsulated by the retry strategy.</param>
+    /// <param name="callerMemberName">Member name of the caller which will be logged when a retry is performed.</param>
+    /// <param name="callerFilePath">Path of the caller which will be used to get the class name to be logged when a retry is performed.</param>
+    /// <returns>The instance of <see cref="ValueTask"/> that represents the asynchronous execution.</returns>
+    public async ValueTask ExecuteAsync(Func<ResilienceContext, ValueTask> callback, [CallerMemberName] string callerMemberName = "", [CallerFilePath] string callerFilePath = "")
+    {
+        ResilienceContext context = ResilienceContextPool.Shared.Get();
+
+        try
+        {
+            // It is necessary to store the caller member name and caller file name to correctly log on retry.
+            context.Properties.Set(CallerMemberNamePropertyKey, callerMemberName);
+            context.Properties.Set(CallerFileNamePropertyKey, Path.GetFileNameWithoutExtension(callerFilePath));
+
+            // ATENTION: use async/await to correctly perform finally block.
+            await _pipeline.ExecuteAsync(callback, context).ConfigureAwait(false);
+        }
+        finally
+        {
+            ResilienceContextPool.Shared.Return(context);
+        }
+    }
+}
