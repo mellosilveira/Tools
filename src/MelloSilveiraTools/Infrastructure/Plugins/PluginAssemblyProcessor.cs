b34045c9dbdbbbcdc24ee5bb7a7cb0a8619a32f8
Bruno Silveira <57352425+M3110@users.noreply.github.com>
2026-04-18 21:06:42 -0300

    Fixed errors on classe PluginAssemblyProcessor.

--- src/MelloSilveiraTools/Infrastructure/Plugins/PluginAssemblyProcessor.cs
+++ src/MelloSilveiraTools/Infrastructure/Plugins/PluginAssemblyProcessor.cs
@@ -17,12 +17,12 @@ public class PluginAssemblyProcessor(
         });
 
     public PluginInfo GetInfo(PluginAssemblyInfo assemblyInfo)
-        => cache.GetOrAdd(assemblyInfo.Descriptor.Name, assemblyInfo.Descriptor.Version, () => new(assemblyInfo));
+        => cache.GetOrAdd(assemblyInfo.Name, assemblyInfo.Version, () => new(assemblyInfo));
 
     public PluginInfo ProcessTypes(PluginAssemblyInfo assemblyInfo, PluginRegistrationContext context)
     {
-        string name = assemblyInfo.Descriptor.Name;
-        PluginVersion version = assemblyInfo.Descriptor.Version;
+        string name = assemblyInfo.Name;
+        PluginVersion version = assemblyInfo.Version;
         PluginInfo typeInfo = cache.GetOrAdd(name, version, () => new(assemblyInfo));
 
         foreach (Type type in assemblyInfo.ProcessableTypes)
